7a9edc657dbbbf353b8b14763897f6df005247f4
Mikołaj Kuranowski <mkuranowski@gmail.com>
2024-11-30 13:43:17 +0100

    build.zig: provide a module, not a static library

--- build.zig
+++ build.zig
@@ -4,25 +4,23 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "csv",
+    _ = b.addModule("csv", .{
         .root_source_file = b.path("csv.zig"),
         .target = target,
         .optimize = optimize,
     });
-    b.installArtifact(lib);
 
-    const lib_unit_tests = b.addTest(.{
+    const lib_test = b.addTest(.{
         .root_source_file = b.path("csv.zig"),
         .target = target,
         .optimize = optimize,
     });
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    const lib_test_run = b.addRunArtifact(lib_test);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&lib_test_run.step);
 
     const lib_docs = b.addInstallDirectory(.{
-        .source_dir = lib.getEmittedDocs(),
+        .source_dir = lib_test.getEmittedDocs(),
         .install_dir = .prefix,
         .install_subdir = "docs",
     });
