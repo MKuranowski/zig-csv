3340edb5ff7d04c143d683f3da10f63489be38a1
Mikołaj Kuranowski <mkuranowski@gmail.com>
2025-10-11 19:55:30 +0200

    build.zig: upgrade to Zig 0.15.1

--- build.zig
+++ build.zig
@@ -4,26 +4,21 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("csv", .{
+    const module = b.addModule("csv", .{
         .root_source_file = b.path("csv.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const lib_test = b.addTest(.{
-        .root_source_file = b.path("csv.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    const lib_test = b.addTest(.{ .root_module = module });
     const lib_test_run = b.addRunArtifact(lib_test);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&lib_test_run.step);
 
-    const lib_static = b.addStaticLibrary(.{
+    const lib_static = b.addLibrary(.{
         .name = "csv",
-        .root_source_file = b.path("csv.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = module,
+        .linkage = .static,
     });
     const lib_docs = b.addInstallDirectory(.{
         .source_dir = lib_static.getEmittedDocs(),
